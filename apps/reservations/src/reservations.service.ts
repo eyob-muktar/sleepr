7167e94bdf79457ad26598a6def3d9c415b80b32
eyob-muktar <eyobmuktar4@gmail.com>
2023-12-04 16:19:35 +0300

    feat: added reservation application

---
+++ apps/reservations/src/reservations.service.ts
@@ -0,0 +1,49 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { ReservationsRepository } from './reservations.repository';
+import { PAYMENTS_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { map } from 'rxjs';
+
+@Injectable()
+export class ReservationsService {
+  constructor(
+    private readonly reservationRepository: ReservationsRepository,
+    @Inject(PAYMENTS_SERVICE) private readonly paymentsService: ClientProxy,
+  ) {}
+
+  async create(createReservationDto: CreateReservationDto, userId: string) {
+    return this.paymentsService
+      .send('create_charge', createReservationDto.charge)
+      .pipe(
+        map((res) => {
+          return this.reservationRepository.create({
+            ...createReservationDto,
+            timestamp: new Date(),
+            userId,
+            invoiceId: res.id,
+          });
+        }),
+      );
+  }
+
+  async findAll() {
+    return this.reservationRepository.find({});
+  }
+
+  async findOne(id: string) {
+    return this.reservationRepository.findOne({ id });
+  }
+
+  async update(id: string, updateReservationDto: UpdateReservationDto) {
+    return this.reservationRepository.findOneAndUpdate(
+      { id },
+      { $set: updateReservationDto },
+    );
+  }
+
+  async remove(id: string) {
+    return this.reservationRepository.findOneAndDelete({ id });
+  }
+}
